17ecf8c8a9a73bd8342b162810f003884b91f36c
Nirmala Kadali <lakshminirmalakadali6@gmail.com>
2024-02-07 11:59:43 +0530

    prev

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import json
-from flask import Flask, request, jsonify
-import requests
-import os
+from flask import send_from_directory,Flask, request, jsonify
+import os 
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,33 +8,17 @@ def index():
     return send_from_directory('.', 'index.html')
 
 @app.route('/add_condition', methods=['POST'])
+
 def add_condition():
-    request_data = request.json
-    json_url = request_data.get('https://drive.google.com/file/d/1fSy-Fd248OZzY-80v5uqrol3wfWlUxNB/view?usp=sharing')
-    
-    if not json_url:
-        return jsonify({"error": "JSON URL not provided"}), 400
-
-    # Fetch JSON data from the provided URL
-    try:
-        response = requests.get(json_url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        new_condition = response.json()
-    except Exception as e:
-        return jsonify({"error": f"Failed to fetch JSON data: {str(e)}"}), 500
-
-    # Path to the local config file
+    new_condition = request.json
     config_path = os.path.join(os.path.dirname(__file__), 'config.json')
 
-    # Read existing data from the local config file
-    with open(json_url, 'r') as file:
+    with open(config_path, 'r') as file:
         data = json.load(file)
 
-    # Append new condition to the existing data
     data['conditions'].append(new_condition)
 
-    # Write updated data back to the local config file
-    with open(json_url, 'w') as file:
+    with open(config_path, 'w') as file:
         json.dump(data, file, indent=4)
 
     return jsonify({"message": "Condition added successfully"})
